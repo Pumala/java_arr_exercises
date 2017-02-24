54601e165052414125b1a53722bbcc7603cca08b
Carolyn Lam <caro_lyn_@hotmail.com>
2017-02-23 23:56:54 -0500

    add curr_lowest_num_in_arr logic

--- src/arr_exercises/return_nums_in_arr.java
+++ src/arr_exercises/return_nums_in_arr.java
@@ -6,6 +6,7 @@ public class return_nums_in_arr {
 		// TODO Auto-generated method stub
 		int [] nums = {9, 4, 10, 3, 15, 8};
 		returnHighestNumInArr(nums);
+		returnLowestNumInArr(nums);
 	}
 
 	public static void returnHighestNumInArr(int[] arr) {
@@ -16,6 +17,16 @@ public class return_nums_in_arr {
 			}
 		}
 		System.out.println(currHighestNum);
+	}
+	
+	public static void returnLowestNumInArr(int[] arr) {
+		int currLowestNum = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] < currLowestNum) {
+				currLowestNum = arr[i];
+			}
+		}
+		System.out.println(currLowestNum);
 	}
 	
 }
